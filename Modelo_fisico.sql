7a9b895c021f1d6814b183091cf31e3046de07e0
Vinicius dos Santos Felix <vsf2@cin.ufpe.br>
2025-03-14 21:52:44 -0300

    Update Modelo_fisico.sql

--- Modelo_fisico.sql
+++ Modelo_fisico.sql
@@ -1,4 +1,3 @@
--- Tabela Funcionário
 CREATE TABLE Funcionario (
     CPF VARCHAR2(11) PRIMARY KEY,
     Telefone VARCHAR2(20),
@@ -9,7 +8,6 @@ CREATE TABLE Funcionario (
     CONSTRAINT FK_Funcionario_Chefe FOREIGN KEY (CPF_Chefe) REFERENCES Funcionario(CPF)
 );
 
--- Tabela Hotel
 CREATE TABLE Hotel (
     CNPJ VARCHAR2(14) PRIMARY KEY,
     Nome VARCHAR2(100),
@@ -18,7 +16,6 @@ CREATE TABLE Hotel (
     CONSTRAINT FK_Hotel_Gerente FOREIGN KEY (CPF_Gerente) REFERENCES Funcionario(CPF)
 );
 
--- Tabela Fones (Hotel pode ter vários telefones)
 CREATE TABLE Fones (
     CNPJ VARCHAR2(14),
     Fone VARCHAR2(20),
@@ -26,7 +23,19 @@ CREATE TABLE Fones (
     CONSTRAINT FK_Fones_Hotel FOREIGN KEY (CNPJ) REFERENCES Hotel(CNPJ)
 );
 
--- Tabela Quarto
+CREATE TABLE Setor (
+    ID_Setor NUMBER PRIMARY KEY,
+    Nome VARCHAR2(100),
+    CNPJ VARCHAR2(14) NOT NULL,
+    CONSTRAINT FK_Setor_Hotel FOREIGN KEY (CNPJ) REFERENCES Hotel(CNPJ)
+);
+
+CREATE TABLE Cliente (
+    CPF VARCHAR2(11) PRIMARY KEY,
+    Telefone VARCHAR2(20),
+    Nome VARCHAR2(100)
+);
+
 CREATE TABLE Quarto (
     ID_Quarto NUMBER,
     CNPJ VARCHAR2(14),
@@ -37,16 +46,12 @@ CREATE TABLE Quarto (
     CONSTRAINT FK_Quarto_Hotel FOREIGN KEY (CNPJ) REFERENCES Hotel(CNPJ)
 );
 
--- Tabela Comodidade
 CREATE TABLE Comodidade (
     ID_Com NUMBER(10) PRIMARY KEY,
     Descricao VARCHAR2(100),
     Custo_Ad NUMBER(6,2)
 );
 
-
-
--- Tabela Reserva
 CREATE TABLE Reserva (
     ID_Res NUMBER PRIMARY KEY,
     Check_In DATE,
@@ -56,25 +61,9 @@ CREATE TABLE Reserva (
     Valor_Total NUMBER(10,2)
 );
 
--- Tabela Cliente
-CREATE TABLE Cliente (
-    CPF VARCHAR2(11) PRIMARY KEY,
-    Telefone VARCHAR2(20),
-    Nome VARCHAR2(100)
-);
-
--- Tabela Setor
-CREATE TABLE Setor (
-    ID_Setor VARCHAR2(5) PRIMARY KEY,
-    Nome VARCHAR2(100),
-    CNPJ VARCHAR2(14) NOT NULL,
-    CONSTRAINT FK_Setor_Hotel FOREIGN KEY (CNPJ) REFERENCES Hotel(CNPJ)
-);
-
--- Tabela Faz_reserva (Relaciona Reserva, Quarto e Cliente)
 CREATE TABLE Faz_Reserva (
-    ID_Res VARCHAR2(5),
-    ID_Quarto VARCHAR2(5),
+    ID_Res NUMBER,
+    ID_Quarto NUMBER,
     CNPJ VARCHAR2(14),
     CPF_Cli VARCHAR2(11) NOT NULL,
     PRIMARY KEY (ID_Res, ID_Quarto, CNPJ),
@@ -83,7 +72,15 @@ CREATE TABLE Faz_Reserva (
     CONSTRAINT FK_FazReserva_Cliente FOREIGN KEY (CPF_Cli) REFERENCES Cliente(CPF)
 );
 
--- Tabela Pagamento
+CREATE TABLE Quarto_Tem_Comodidade (
+    ID_Comodidade NUMBER,
+    ID_Quarto NUMBER,
+    CNPJ VARCHAR2(14),
+    PRIMARY KEY (ID_Comodidade, ID_Quarto, CNPJ),
+    CONSTRAINT FK_QTC_Comodidade FOREIGN KEY (ID_Comodidade) REFERENCES Comodidade(ID_Com),
+    CONSTRAINT FK_QTC_Quarto FOREIGN KEY (ID_Quarto, CNPJ) REFERENCES Quarto(ID_Quarto, CNPJ)
+);
+
 CREATE TABLE Pagamento (
     ID_Pag NUMBER PRIMARY KEY,
     Data DATE,
@@ -95,17 +92,6 @@ CREATE TABLE Pagamento (
     CONSTRAINT FK_Pagamento_FazReserva FOREIGN KEY (ID_Res, ID_Quarto, CNPJ) REFERENCES Faz_Reserva(ID_Res, ID_Quarto, CNPJ)
 );
 
--- Tabela Quarto_Tem_Comodidade
-CREATE TABLE Quarto_Tem_Comodidade (
-    ID_Comodidade NUMBER,
-    ID_Quarto NUMBER,
-    CNPJ VARCHAR2(14),
-    PRIMARY KEY (ID_Comodidade, ID_Quarto, CNPJ),
-    CONSTRAINT FK_QTC_Comodidade FOREIGN KEY (ID_Comodidade) REFERENCES Comodidade(ID_Com),
-    CONSTRAINT FK_QTC_Quarto FOREIGN KEY (ID_Quarto, CNPJ) REFERENCES Quarto(ID_Quarto, CNPJ)
-);
-
--- Tabela Hotel_Tem_Comodidade
 CREATE TABLE Hotel_Tem_Comodidade (
     ID_Comodidade NUMBER,
     CNPJ VARCHAR2(14),
@@ -114,7 +100,6 @@ CREATE TABLE Hotel_Tem_Comodidade (
     CONSTRAINT FK_HTC_Hotel FOREIGN KEY (CNPJ) REFERENCES Hotel(CNPJ)
 );
 
--- Tabela Trabalha (Relaciona Funcionário e Setor)
 CREATE TABLE Trabalha (
     CPF_Func VARCHAR2(11),
     Data_Admissao DATE,
