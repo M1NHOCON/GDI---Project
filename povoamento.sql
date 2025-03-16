7ad4943c28c8bf5615553105e8c45b69c3c6a35d
Paulo Messias <114231448+pmn12@users.noreply.github.com>
2025-03-15 22:02:25 -0300

    Update povoamento.sql

--- povoamento.sql
+++ povoamento.sql
@@ -1,4 +1,3 @@
--- 1. Tabela: Funcionario (7 funcionários, sendo 1 gerente)
 INSERT INTO Funcionario (CPF, Telefone, Nome, Salario, Cargo, CPF_Chefe) VALUES 
   ('1', '11', 'Carlos Silva', 4000.00, 'Gerente', NULL),
   ('2', '11', 'Vinicius Feliz', 3000.00, 'Gerente', NULL),
@@ -17,7 +16,7 @@ INSERT INTO Hotel (CNPJ, Nome, Endereco, CPF_Gerente) VALUES
 -- 3. Tabela: Fones (telefones dos hotéis)
 INSERT INTO Fones (CNPJ, Fone) VALUES 
   ('1', '11'),
-  ('2', '22')
+  ('2', '22'),
   ('1', '33');
 
 -- 4. Tabela: Quarto (3 quartos em cada hotel – um de cada tipo)
@@ -32,7 +31,6 @@ INSERT INTO Quarto (ID_Quarto, CNPJ, Tipo, Capacidade, Valor) VALUES
 -- 5. Tabela: Comodidade 
 INSERT INTO Comodidade (ID_Com, Descricao) VALUES 
   (1, 'Jacuzzi'),
-  (2, 'Decoração Romântica'),
   (3, 'Minibar Liberado'),
   (4, 'SPA'),
   (5, 'Academia'),
@@ -52,18 +50,18 @@ INSERT INTO Cliente (CPF, Telefone, Nome) VALUES
 
 -- 8. Tabela: Setor (3 setores)
 INSERT INTO Setor (ID_Setor, Nome, CNPJ) VALUES 
-  (01, 'Recepção', '1'),
-  (02, 'Recepção', '2'),
-  (03, 'Limpeza', '1'),
-  (04, 'Limpeza', '2'),
-  (05, 'Administração', '1');
-  (06, 'Administração', '2');
+  (111, 'Recepção', '1'),
+  (222, 'Recepção', '2'),
+  (333, 'Limpeza', '1'),
+  (444, 'Limpeza', '2'),
+  (555, 'Administração', '1'),
+  (666, 'Administração', '2');
 
 -- 9. Tabela: Faz_Reserva
 INSERT INTO Faz_Reserva (ID_Res, ID_Quarto, CNPJ, CPF_Cli) VALUES 
-  (1, 1, '1', '8'),
-  (2, 1, '2', '9'),
-  (2, 2, '2', '9');
+  (1, 1, '1', '10'),
+  (2, 1, '2', '11'),
+  (2, 2, '2', '11');
 
 -- 10. Tabela: Pagamento
 INSERT INTO Pagamento (ID_Pag, Data, Valor, Forma_Pag, ID_Res, ID_Quarto, CNPJ) VALUES 
@@ -74,10 +72,8 @@ INSERT INTO Pagamento (ID_Pag, Data, Valor, Forma_Pag, ID_Res, ID_Quarto, CNPJ) 
 -- 11. Tabela: Quarto_Tem_Comodidade
 INSERT INTO Quarto_Tem_Comodidade (ID_Comodidade, ID_Quarto, CNPJ) VALUES 
   (1, 3, '1'),
-  (2, 3, '1'),
   (3, 3, '1'),
   (1, 3, '2'),
-  (2, 3, '2'),
   (3, 3, '2');
 
 -- 12. Tabela: Hotel_Tem_Comodidade
@@ -88,9 +84,9 @@ INSERT INTO Hotel_Tem_Comodidade (ID_Comodidade, CNPJ) VALUES
 
 -- 13. Tabela: Trabalha
 INSERT INTO Trabalha (CPF_Func, Data_Admissao, ID_Setor) VALUES 
-  ('4', TO_DATE('2020-01-10','YYYY-MM-DD'), 05),
-  ('7', TO_DATE('2021-06-15','YYYY-MM-DD'), 06),
-  ('3', TO_DATE('2019-04-10','YYYY-MM-DD'), 01),
-  ('6', TO_DATE('2022-03-20','YYYY-MM-DD'), 02),
-  ('5', TO_DATE('2023-07-30','YYYY-MM-DD'), 03),
-  ('8', TO_DATE('2022-05-01','YYYY-MM-DD'), 04),
+  ('4', TO_DATE('2020-01-10','YYYY-MM-DD'), 555),
+  ('7', TO_DATE('2021-06-15','YYYY-MM-DD'), 666),
+  ('3', TO_DATE('2019-04-10','YYYY-MM-DD'), 111),
+  ('6', TO_DATE('2022-03-20','YYYY-MM-DD'), 222),
+  ('5', TO_DATE('2023-07-30','YYYY-MM-DD'), 333),
+  ('8', TO_DATE('2022-05-01','YYYY-MM-DD'), 444);
