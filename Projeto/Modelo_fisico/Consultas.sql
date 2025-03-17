3d08a4793f5b0243799750873a17f7abde8927d8
Paulo Messias <114231448+pmn12@users.noreply.github.com>
2025-03-17 19:48:06 -0300

    Update Consultas.sql

--- Projeto/Modelo_fisico/Consultas.sql
+++ Projeto/Modelo_fisico/Consultas.sql
@@ -1,20 +1,4 @@
-/*Seu projeto deve ter todos os tipos de consultas abaixo
-1-Group by/Having
-2-Junção interna
-3-Junção externa
-4-Semi junção
-5-Anti-junção
-6-Subconsulta do tipo escalar
-7-Subconsulta do tipo linha
-8-Subconsulta do tipo tabela
-9-Operação de conjunto
-
-Atenção: Cada aluno deve fazer ao menos 01 dessas consultas mais 01 procedimento com SQL embutida e parâmetro, função com SQL embutida e parâmetro ou gatilho. 
-*/
---ATENÇÃO - É IMPORTANTE QUE OS GATILHOS RESOLVAM PROBLEMAS REAIS DO NOSSO ESQUEMA(vejam meu exemplo(Paulo))
---SUGESTÃO - Trocar datas de check-in-out para quarto, pois em reserva, reservamos quartos mesmos nos dias que não estaremos neles caso sejam de hoteis diferentes
-
--- Group by/Having - Paulo -- mostra o os nomes e quantidade de reservas dos clientes que fizeram mais de uma reserva
+-- Group by/Having -- mostra o os nomes e quantidade de reservas dos clientes que fizeram mais de uma reserva
 SELECT C.Nome AS Nome_Cliente,COUNT(r.ID_Res) AS Total_Reservas
 FROM Cliente C
 JOIN Faz_Reserva fr ON c.CPF = fr.CPF_Cli
@@ -22,7 +6,7 @@ JOIN Reserva r ON fr.ID_Res = r.ID_Res
 GROUP BY C.Nome
 HAVING COUNT(r.ID_Res) > 1
 
--- Junção interna - Paulo - mostra os hoteis e seus respectivos gerentes
+-- Junção interna - mostra os hoteis e seus respectivos gerentes
 SELECT 
     H.Nome AS Nome_Hotel,
     (F.Nome) AS Nome_Gerente,
@@ -30,14 +14,14 @@ SELECT
 FROM Hotel H
 INNER JOIN Funcionario F ON H.CPF_Gerente = F.CPF;
 
--- Junção externa - Paulo -- mostra todos os quartos e suas comodidades(quando existem)
+-- Junção externa -- mostra todos os quartos e suas comodidades(quando existem)
 SELECT Distinct Q.ID_Quarto,Q.CNPJ, Q.Tipo, Q.VALOR, (QC.ID_COMODIDADE)
 FROM Quarto Q
 LEFT JOIN Quarto_Tem_Comodidade QC 
 ON Q.ID_Quarto = QC.ID_Quarto
 ORDER BY Q.ID_Quarto;
 
--- Semi junção - vinicius -- Clientes que já fizeram reservas
+-- Semi junção -- Clientes que já fizeram reservas
 SELECT DISTINCT c.*
 FROM Cliente c
 WHERE EXISTS (
@@ -46,7 +30,7 @@ WHERE EXISTS (
     WHERE fr.CPF_Cli = c.CPF
 );
 
--- Anti-junção - Paulo -- Mostra as reservas que nao tem nenhum pagamento
+-- Anti-junção -- Mostra as reservas que nao tem nenhum pagamento
 Select R.ID_RES
 from RESERVA R
 Where not exists(
@@ -55,12 +39,12 @@ Where not exists(
    where P.ID_RES = R.ID_RES
 )
 
--- Subconsulta do tipo escalar - Paulo -- (consulta que retorna apenas um valor) - Funcionário com o maior salário
+-- Subconsulta do tipo escalar -- (consulta que retorna apenas um valor) - Funcionário com o maior salário
 SELECT Nome
 FROM Funcionario
 WHERE Salario = (SELECT MAX(Salario) FROM Funcionario);
 
--- Subconsulta do tipo linha - Paulo -- (retorna 1 linha com várias colunas)- reserva com o maior valor
+-- Subconsulta do tipo linha -- (retorna 1 linha com várias colunas)- reserva com o maior valor
 SELECT * 
 FROM Reserva
 WHERE ID_Res = (
@@ -70,28 +54,23 @@ WHERE ID_Res = (
                          FROM Reserva)
 );
 
--- Subconsulta do tipo tabela - Paulo - (retorna varias colunas e linhas) retorna os setores com mais funcionários
-SELECT S.ID_Setor, S.Nome, COUNT(T.CPF_Func) AS Num_Funcionarios
-FROM Setor S
-JOIN Trabalha T ON S.ID_Setor = T.ID_Setor
-GROUP BY S.ID_Setor, S.Nome
-HAVING COUNT(T.CPF_Func) = (
-    SELECT MAX(Num_Func)
-    FROM (
-        SELECT COUNT(CPF_Func) AS Num_Func
-        FROM Trabalha
-        GROUP BY ID_Setor
-    )
+-- Subconsulta do tipo tabela --Listar funcionários que trabalham em setores específicos, nesse caso, 111 e 222
+SELECT *
+FROM Funcionario
+WHERE CPF IN (
+  SELECT CPF_Func 
+  FROM Trabalha 
+  WHERE ID_Setor IN (111,222)
 );
 
--- Operação de conjunto - Paulo - Retorna os cpf's e nomes de todas as pessoas, clientes + func
+-- Operação de conjunto - Retorna os cpf's e nomes de todas as pessoas, clientes + func
 SELECT CPF, Nome, 'Funcionário' AS Tipo
 FROM Funcionario
 UNION
 SELECT CPF, Nome, 'Cliente' AS Tipo
 FROM Cliente;
 
---Procedimento - Paulo - Insere um novo Funcionário
+--Procedimento - Insere um novo Funcionário
 CREATE OR REPLACE PROCEDURE InserirFuncionario(
     p_CPF IN VARCHAR2,
     p_Telefone IN VARCHAR2,
@@ -111,7 +90,7 @@ BEGIN
 END;
 ---------------------------------
 
---Função - Paulo - retorna o salário de um funcionário
+--Função - retorna o salário de um funcionário
 CREATE OR REPLACE FUNCTION ObterSalario(
     p_CPF IN VARCHAR2
 ) RETURN NUMBER AS
@@ -127,7 +106,7 @@ END;
 SELECT ObterSalario('1') FROM DUAL;
 --------------------------------------------
 
---Gatilho - Paulo - Impede que o funcionário tenha um chefe que trabalha em outro hotel
+--Gatilho - Impede que o funcionário tenha um chefe que trabalha em outro hotel
 CREATE OR REPLACE TRIGGER ChefeMesmoHotel
 BEFORE INSERT OR UPDATE OF CPF_Chefe ON Funcionario
 FOR EACH ROW
@@ -153,69 +132,3 @@ BEGIN
         RAISE_APPLICATION_ERROR(-20001, 'O chefe deve trabalhar no mesmo hotel que o funcionário.');
     END IF;
 END;
-
-
--- Consulta 1 -- Vinicius -- Total de reservas por cliente com valor total superior a 1000
-SELECT CPF_Cli, SUM(r.Valor_Total) AS Total_Gasto
-FROM Faz_Reserva fr
-JOIN Reserva r ON fr.ID_Res = r.ID_Res
-GROUP BY CPF_Cli
-HAVING SUM(r.Valor_Total) > 1000;
-
--- Consulta 2 -- Vinicius -- Pagamentos com detalhes de reserva
-SELECT p.ID_Pag, p.Valor, r.Status
-FROM Pagamento p INNER JOIN 
-      Reserva r ON p.ID_Res = r.ID_Res;
-
--- Consulta 3 -- vinicius -- Setores e funcionários alocados (incluindo setores sem funcionários)
-SELECT s.Nome, f.Nome
-FROM Setor s LEFT JOIN
-      Trabalha t ON s.ID_Setor = t.ID_Setor LEFT JOIN
-      Funcionario f ON t.CPF_Func = f.CPF;
-
--- Consulta 4 -- vinicius -- Clientes que já fizeram reservas
-SELECT DISTINCT c.*
-FROM Cliente c
-WHERE EXISTS (
-    SELECT fr.ID_RES
-    FROM Faz_Reserva fr 
-    WHERE fr.CPF_Cli = c.CPF
-);
-
--- Consulta 5 -- vinicius -- Clientes que nunca fizeram reservas
-SELECT *
-FROM Cliente c
-WHERE NOT EXISTS (
-    SELECT fr.ID_RES 
-    FROM Faz_Reserva fr 
-    WHERE fr.CPF_Cli = c.CPF
-);
--- Consulta 6 -- vinicius -- Retorna o Faturamento total de Reservas
-SELECT (SELECT SUM(Valor_Total) FROM Reserva) AS Total_Reservas 
-FROM DUAL;
-
--- Consulta 7 -- vinicius --Detalhes do funcionário com maior salário
-SELECT *
-FROM Funcionario f
-WHERE f.SALARIO = (
-  SELECT MAX(f2.SALARIO)
-  FROM Funcionario f2
-);
-
--- Consulta 8 -- vinicius --Listar funcionários que trabalham em setores específicos, nesse caso, 1, 2 e 3
-SELECT *
-FROM Funcionario
-WHERE CPF IN (
-  SELECT CPF_Func 
-  FROM Trabalha 
-  WHERE ID_Setor IN (1, 2, 3)
-);
-
--- Consulta 9 -- vinicius -- MINUS: Quartos que nunca foram reservados
-SELECT ID_Quarto FROM Quarto
-MINUS
-SELECT ID_Quarto FROM Faz_Reserva;
-
-
---confusaosinha em faz_reserva, id_res pode se repetir desde que seja com outro id_quarto ou cnpj.
---porem id_res em reserva não pode se repetir, então vai estar se referindo sempre aos mesmos valores de atributos
