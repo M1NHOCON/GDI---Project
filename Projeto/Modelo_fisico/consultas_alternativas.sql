f22c3e687a953d9d5439e646c849d6ed2e1df65e
Paulo Messias <114231448+pmn12@users.noreply.github.com>
2025-03-19 20:34:26 -0300

    Update consultas_alternativas.sql

--- Projeto/Modelo_fisico/consultas_alternativas.sql
+++ Projeto/Modelo_fisico/consultas_alternativas.sql
@@ -3,3 +3,11 @@ select c.nome, (select count(*) from Faz_Reserva fr where fr.CPF_Cli = c.cpf)
 from Cliente c
 where (select count(*) from Faz_Reserva fr where fr.CPF_Cli = c.cpf)>1
 
+-----2-----
+Select h.Nome as hotel , (select f.NOME from funcionario f where f.CPF = h.CPF_GERENTE)  as gerente
+from hotel h
+where h.CPF_GERENTE IN (
+    select f.cpf
+    from FUNCIONARIO f
+    where f.cpf = h.CPF_GERENTE
+)
